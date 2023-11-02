b9d7f718dc9c9ee0434667a81bad3a19f2c070a6
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-11-02 19:34:18 -0300

    README wip

--- README-StylesColorShifter-MDH.mm
+++ README-StylesColorShifter-MDH.mm
@@ -22,15 +22,23 @@
                 <script>(node.findAll() - node)?.any{it.style.name == &apos;Tarea pendiente&apos;} </script>
             </script_condition>
         </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="file" LAST="false">
+            <script_condition>
+                <script>{node.link.file &amp;&amp; !node.link.uri?.fragment}</script>
+            </script_condition>
+        </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="modifiedFile" LAST="false">
+            <attribute_contains_condition ATTRIBUTE="modifiedFile" VALUE="true"/>
+        </conditional_style>
     </conditional_styles>
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/vis01%20light_nord_template%20vEdo%20v2.mm" fit_to_viewport="false" mapUsesOwnSaveOptions="true" save_folding="save_folding_if_map_is_changed" save_last_visited_node="default" save_modification_times="false"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" mapUsesOwnSaveOptions="true" save_modification_times="false" save_last_visited_node="default" show_note_icons="true" associatedTemplateLocation="template:/vis01%20light_nord_template%20vEdo%20v2.mm" save_folding="save_folding_if_map_is_changed" fit_to_viewport="false" MDI_template="v0.0.13"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" ID="ID_118736178" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="5 px" SHAPE_VERTICAL_MARGIN="2 px" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="2 px" COMMON_HGAP_QUANTITY="14 pt" CHILD_NODES_LAYOUT="AUTO">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#bf5d3f" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTINCLINATION="45 pt;-12.75 pt;" ENDINCLINATION="57 pt;30 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#bf5d3f" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTINCLINATION="45 pt;-12 pt;" ENDINCLINATION="57 pt;30 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="Dialog" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="horizontal" COLOR="#2e3440" WIDTH="1" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
@@ -123,6 +131,49 @@ blockquote {
 <icon BUILTIN="emoji-1F6A9"/>
 <font BOLD="true"/>
 </stylenode>
+<stylenode TEXT="baseFolder" ICON_SIZE="20 pt" BACKGROUND_COLOR="#ffeca9">
+<icon BUILTIN="emoji-1F4BD"/>
+<font BOLD="true"/>
+</stylenode>
+<stylenode TEXT="newFolderImport" ICON_SIZE="16 pt" BACKGROUND_COLOR="#ffeca9">
+<icon BUILTIN="emoji-1F4BE"/>
+<font BOLD="true"/>
+</stylenode>
+<stylenode TEXT="freshNew" ICON_SIZE="16 pt" BACKGROUND_COLOR="#ace500">
+<icon BUILTIN="emoji-1F195"/>
+</stylenode>
+<stylenode TEXT="movedRenamed" ICON_SIZE="16 pt" BACKGROUND_COLOR="#ace500">
+<icon BUILTIN="emoji-1F500"/>
+</stylenode>
+<stylenode TEXT="file" BACKGROUND_COLOR="#bcc6e0" FORMAT="NO_FORMAT" BORDER_WIDTH="2.5 px">
+<font NAME="Consolas"/>
+</stylenode>
+<stylenode TEXT="file_folder" BORDER_WIDTH="3 px">
+<icon BUILTIN="emoji-1F4C1"/>
+</stylenode>
+<stylenode TEXT="missing" BACKGROUND_COLOR="#f28bb3" BORDER_WIDTH="3 px">
+<icon BUILTIN="broken-line"/>
+</stylenode>
+<stylenode TEXT="modifiedFile" BACKGROUND_COLOR="#ffcc00" BORDER_WIDTH="4 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#00659b">
+<icon BUILTIN="emoji-002A-20E3"/>
+<font ITALIC="true"/>
+</stylenode>
+<stylenode TEXT="locked" COLOR="#e1e1e1" BACKGROUND_COLOR="#6f4e4e" BORDER_WIDTH="2.5 px">
+<icon BUILTIN="emoji-1F512"/>
+</stylenode>
+<stylenode TEXT="file_folder_with_icon">
+<icon BUILTIN="emoji-1F4CD"/>
+</stylenode>
+<stylenode TEXT="notMovedRenamed" BACKGROUND_COLOR="#f28bb3" BORDER_WIDTH="3 px">
+<icon BUILTIN="emoji-26D4"/>
+</stylenode>
+<stylenode TEXT="moveToTrash" BACKGROUND_COLOR="#e0e000">
+<icon BUILTIN="emoji-1F6AE"/>
+<font STRIKETHROUGH="false" ITALIC="true"/>
+</stylenode>
+<stylenode TEXT="trashFolder" BACKGROUND_COLOR="#e0e000" STYLE="rectangle" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#c61b26">
+<icon BUILTIN="emoji-1F5D1"/>
+</stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ID="ID_1209359852" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="15 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="5 cm" MIN_WIDTH="3 cm">
@@ -177,8 +228,223 @@ blockquote {
 <attribute NAME="ignoreHeaderImageObjects" VALUE="false"/>
 <richcontent TYPE="NOTE">
 <text>= edofro.MarkDownHelper.MDH.document(node)</text></richcontent>
-<node TEXT="Dummy readme file" ID="ID_1673406844">
-<node TEXT="just a **dummy** markdown file" ID="ID_1896388321"/>
+<node TEXT="Styles Color Shifter" ID="ID_1673406844">
+<node TEXT="This add-on allows to &quot;shift&quot; the colors of the template to adapt the mind map to the user&apos;s taste and/or to the style required by the mindmaps content." ID="ID_1948288289"/>
+<node TEXT="horizontal line" STYLE_REF="MarkdownHelperNode" ID="ID_256940880"><richcontent TYPE="NOTE">
+<text>-----</text></richcontent>
+</node>
+<node TEXT="Status" ID="ID_276284645">
+<node TEXT="table" STYLE_REF="MarkdownHelperNode" ID="ID_201597321"><richcontent TYPE="NOTE">
+<text>= edofro.MarkDownHelper.MDH.table(node)</text></richcontent>
+<node TEXT="r" ID="ID_757284198">
+<node TEXT="" ID="ID_941743014"/>
+<node TEXT="comment" ID="ID_1959229111"/>
+</node>
+<node TEXT="r" ID="ID_1855539080">
+<node TEXT="Version" ID="ID_543179215"/>
+<node TEXT="0.0.1" ID="ID_108326171"/>
+</node>
+<node TEXT="r" ID="ID_1424474120">
+<node TEXT="Status (1)" ID="ID_1509013490"/>
+<node TEXT="Working" ID="ID_261307688"/>
+</node>
+<node TEXT="r" ID="ID_559245927">
+<node TEXT="Usefulness (2)" ID="ID_1025690288"/>
+<node TEXT="I will report after a while" ID="ID_1687120576"/>
+</node>
+<node TEXT="r" ID="ID_235623352">
+<node TEXT="my opinion" ID="ID_865838450"/>
+<node TEXT="Interesting, funny" ID="ID_1438485510"/>
+</node>
+</node>
+<node TEXT="I added this table to give some more information about the addon, its development status and my own impression of its usefulness." ID="ID_1141638869"/>
+<node TEXT="Table explanation" ID="ID_634961425">
+<icon BUILTIN="emoji-1F53B"/>
+<node TEXT="list" STYLE_REF="MarkdownHelperNode" POSITION="bottom_or_right" ID="ID_927087572">
+<icon BUILTIN="emoji-1F522"/>
+<richcontent TYPE="NOTE">
+<text>= edofro.MarkDownHelper.MDH.list(node)</text></richcontent>
+<node TEXT="Status" ID="ID_905844243">
+<icon BUILTIN="emoji-1F537"/>
+<node TEXT="Status of the project" ID="ID_257295144"/>
+<node TEXT="Like:" ID="ID_1940614169">
+<node TEXT="Designing" ID="ID_1760049965"/>
+<node TEXT="Testing ideas" ID="ID_947205351"/>
+<node TEXT="Work in progress" ID="ID_1492176616"/>
+<node TEXT="Working" ID="ID_818961454"/>
+<node TEXT="Mature" ID="ID_1012751304"/>
+</node>
+</node>
+<node TEXT="Usefulness" ID="ID_813051651">
+<icon BUILTIN="emoji-1F537"/>
+<node TEXT="This point expresses how important this tool is in the mind mapping process for me" ID="ID_956973658"/>
+<node TEXT="Like:" ID="ID_503867148">
+<icon BUILTIN="emoji-1F522"/>
+<node TEXT="A must! I can&apos;t use Freeplane without it" ID="ID_1486675140"/>
+<node TEXT="Very helpful. I use it every day" ID="ID_1952798703"/>
+<node TEXT="Useful. It works for me when I need it" ID="ID_1481854434"/>
+<node TEXT="Helps but is not necessary" ID="ID_7042957"/>
+<node TEXT="I don&apos;t use it much" ID="ID_1935995437"/>
+<node TEXT="I don&apos;t use it" ID="ID_1668555356"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="horizontal line" STYLE_REF="MarkdownHelperNode" ID="ID_276704639"><richcontent TYPE="NOTE">
+<text>-----</text></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="resources" STYLE_REF="baseFolder" POSITION="bottom_or_right" ID="ID_834164356" LINK="resources/">
+<attribute NAME="nameFilter" VALUE=""/>
+<attribute NAME="maxDepth" VALUE="-1" OBJECT="org.freeplane.features.format.FormattedNumber|-1|#0.####"/>
+<attribute NAME="linkType" VALUE="1" OBJECT="org.freeplane.features.format.FormattedNumber|1|#0.####"/>
+<attribute NAME="markWhenMoved" VALUE="0" OBJECT="org.freeplane.features.format.FormattedNumber|0|#0.####"/>
+<attribute NAME="checkIfReallyBroken" VALUE="0" OBJECT="org.freeplane.features.format.FormattedNumber|0|#0.####"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/markdown">
+<text>
+# MDI:  parameters
+The import of files and folders can be adapted by providing various options in the attributes of the BaseFolder node:
+
+-----------------------------------------------------
+# MDI: nameFilter:
+A filter to perform on the name of traversed files. If set, only files which match are brought. 
+This option allowes four types of inputs:
+1. nothing (empty) means no filtering (default) 
+2. regex
+  - example: `~/.*\.mp3/ `
+3. 'simplified' regex
+  - example: `~.*\.mp3` 
+4. string with *
+  - example: `*.mp3`   
+    (equivalent to regex      `~/(?i).*\.mp3/`  )
+5. list of strings with '*' and ';'
+  - example: ` *.mp3;*.png `   
+    (equivalent to regex: ` ~/(?i)(.*\.mp3|.*\.png)/ `  )
+
+-----------------------------------------------------
+
+# MDI:  maxDepth:
+
+The maximum number of directory levels when recursing   
+(default is -1 which means no limit, set to 0 for no recursion)
+
+
+-----------------------------------------------------
+
+# MDI:  linkType:
+
+Define if you want to use Absolute or Relative   
+links for files and folders.
+
+ set to:   
+
+0 : to use Absolute links
+
+1 : to use Relative links
+
+-----------------------------------------------------
+   
+# MDI:  markWhenMoved:
+
+change styles to moved/renamed file Nodes 
+
+set to:   
+ 
+0  : to change style only if node hasn't a previous one (default),
+
+1  : to allways change the style,
+
+-1 : to never change the style
+
+-----------------------------------------------------
+
+ 
+# MDI:  checkIfReallyBroken:
+Check if existing nodes pointing to filtered files still exist.   
+This option is only useful if you defined a nameFilter before 
+but in the map there are also some files that doesn't match 
+this filter definition 
+(for example if you brought them manually or import them 
+before the actual namefilter setting)  
+
+- default is 0 which means don't check --&gt; Mark node as missing also if it doesn't match the current filter,
+
+- set to 1 to extra check if a not matching file still exists in drive 
+
+
+-----------------------------------------------------
+
+</text></richcontent>
+<node TEXT="ColorShifterDialog.mp4" ID="ID_857079340" LINK="resources/ColorShifterDialog.mp4"/>
+<node TEXT="RandomColors.mp4" ID="ID_1365729725" LINK="resources/RandomColors.mp4"/>
+<node TEXT="Test shift colors.mm" ID="ID_777523337" LINK="resources/Test%20shift%20colors.mm"/>
+<node TEXT="new imported files" STYLE_REF="newFolderImport" ID="ID_406625579">
+<attribute NAME="log_MDI" VALUE="2" OBJECT="org.freeplane.features.format.FormattedNumber|2"/>
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Inated:&#xa0;&#xa0;&#xa0;2023-11-02&#xa0;&#xa0;16:47:55
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ------- Files: --------&#xa0;
+    </p>
+    <p>
+      &#xa0;0 node(s) pointing to unexisting/filtered files (marked as 'broken')
+    </p>
+    <p>
+      &#xa0;0 link(s) corrected in nodes
+    </p>
+    <p>
+      &#xa0;3 new file(s) imported as node(s)&#xa0;
+    </p>
+    <p>
+      &#xa0;0 node(s) moved/renamed in drive
+    </p>
+    <p>
+      &#xa0;0 node(s) couldn't be moved/renamed in drive (marked as 'notMovedRenamed')
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      ------- Folders: --------&#xa0;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      5.4 seconds
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      =====================================
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      No failed operation in drive
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
 </node>
 </node>
